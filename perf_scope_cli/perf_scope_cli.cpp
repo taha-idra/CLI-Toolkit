90b67a600c1ec4a3991fb58b56403d89299522fc
Taha Idra <taha.idra@mailfence.com>
2025-01-22 12:06:00 +0200

    Update perf_scope_cli.cpp

--- perf_scope_cli/perf_scope_cli.cpp
+++ perf_scope_cli/perf_scope_cli.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <chrono>
+#include <thread> // للإيقاف المؤقت
 
+// Project: PerfScopeCLI
+// Description: Performance Metrics Tool
+// Author: Taha Idra
+// Version: 0.1
+
+int main() {
+    //
+    auto start = std::chrono::high_resolution_clock::now();
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+    std::cout << "وقت التنفيذ: " << duration.count() << " مللي ثانية" << std::endl;
+    //
+  
+    return 0;
+}
